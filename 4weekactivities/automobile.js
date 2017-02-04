f954b1a9d590e42ee468f368335f1df0af66be5b
harderg <harderg@oregonstate.edu>
2017-02-04 12:31:36 -0800

    add code necessary to complete HW assignment

--- 4weekactivities/automobile.js
+++ 4weekactivities/automobile.js
@@ -14,9 +14,23 @@ var automobiles = [
     new Automobile(2008, "Subaru", "Outback", "Wagon")
     ];
 
+Automobile.prototype.logMe = function(type) {
+    if(type) {
+        console.log(this.year, this.make, this.model, this.type)
+    } else {
+        console.log(this.year, this.make, this.model)
+    }
+}
+
+printCars = function(cars, type) {
+    for (var car in cars) {
+        cars[car].logMe(type);
+    }
+}
+
 /*This function sorts arrays using an arbitrary comparator. You pass it a comparator and an array of objects appropriate for that comparator and it will return a new array which is sorted with the largest object in index 0 and the smallest in the last index*/
-function sortArr( comparator, array ){
-    console.log("Hello World");
+function sortArr(comparator, array){
+    return array.sort(comparator);
 }
 
 /*A comparator takes two arguments and uses some algorithm to compare them. If the first argument is larger or greater than the 2nd it returns true, otherwise it returns false. Here is an example that works on integers*/
@@ -31,42 +45,52 @@ function exComparator( int1, int2){
 /*For all comparators if cars are 'tied' according to the comparison rules then the order of those 'tied' cars is not specified and either can come first*/
 
 /*This compares two automobiles based on their year. Newer cars are "greater" than older cars.*/
-function yearComparator( auto1, auto2){
-    /* your code here*/
+function yearComparator(auto1, auto2){
+    return auto2.year - auto1.year;
 }
 
 /*This compares two automobiles based on their make. It should be case insensitive and makes which are alphabetically earlier in the alphabet are "greater" than ones that come later.*/
-function makeComparator( auto1, auto2){
-    /* your code here*/
+function makeComparator(auto1, auto2){
+    if(auto1.make < auto2.make) return -1;
+    if(auto1.make > auto2.make) return 1;
+    return 0;
 }
 
 /*This compares two automobiles based on their type. The ordering from "greatest" to "least" is as follows: roadster, pickup, suv, wagon, (types not otherwise listed). It should be case insensitive. If two cars are of equal type then the newest one by model year should be considered "greater".*/
-function typeComparator( auto1, auto2){
-    /* your code here*/
-}
+function typeComparator(auto1, auto2){
+    autos = [auto1, auto2];
+    vals = [0, 0];
+    for (var car in autos) {
+        if (autos[car].type == "Roadster"){
+            vals[car] = 4;
+        } else if (autos[car].type == "Pickup") {
+            vals[car] = 3;
+        } else if (autos[car].type == "SUV") {
+            vals[car] = 2;
+        } else if (autos[car].type == "Wagon") {
+            vals[car] = 1;
+        } else {
+            vals[car] = 0;
+        }
+    }
 
-sortArr();
+    return vals[1] - vals[0];
+}
 
 /*Your program should output the following to the console.log, including the opening and closing 5 stars. All values in parenthesis should be replaced with appropriate values. Each line is a seperate call to console.log.
 
-Each line representing a car should be produced via a logMe function. This function should be added to the Automobile class and accept a single boolean argument. If the argument is 'true' then it prints "year make model type" with the year, make, model and type being the values appropriate for the automobile. If the argument is 'false' then the type is ommited and just the "year make model" is logged.
+Each line representing a car should be produced via a logMe function. This function should be added to the Automobile class and accept a single boolean argument. If the argument is 'true' then it prints "year make model type" with the year, make, model and type being the values appropriate for the automobile. If the argument is 'false' then the type is ommited and just the "year make model" is logged.*/
 
-*****
-The cars sorted by year are:
-(year make model of the 'greatest' car)
-(...)
-(year make model of the 'least' car)
+console.log('*****');
+console.log('The cars sorted by year are:');
+printCars(sortArr(yearComparator, automobiles), true);
 
-The cars sorted by make are:
-(year make model of the 'greatest' car)
-(...)
-(year make model of the 'least' car)
+console.log('\nThe cars sorted by make are:');
+printCars(sortArr(makeComparator, automobiles), true);
 
-The cars sorted by type are:
-(year make model type of the 'greatest' car)
-(...)
-(year make model type of the 'least' car)
-*****
+console.log('\nThe cars sorted by type are:');
+printCars(sortArr(typeComparator, automobiles), true);
+console.log('*****');
 
-As an example of the content in the parenthesis:
+/*As an example of the content in the parenthesis:
 1990 Ford F-150 */
